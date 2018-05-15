e37dd7e1b23da9ac9fea09e93878df9f42a855d6
Your Name <you@example.com>
2018-05-15 12:36:35 +0300

    up

---
+++ contact.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+         $("body").animate({
+             scrollTop: 700
+         }, 1200);
+         return false;
+    });
+
+    $(document).on("click", "#return-to-top", function() {
+        $("body").animate(
+          {
+            scrollTop: 0
+          },
+          500
+        );
+        return false;
+      });
+
+$("body").on("scroll",function(){
+    var y = $("body").scrollTop();
+    if (y > 400) {
+      $("#return-to-top").fadeIn(500);
+    } else {
+
+      $("#return-to-top").fadeOut(500);
+    }
+  });
